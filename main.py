0ad7185135830e4e94ad03ad1822dfa9c6e4562c
Martijn Braam <martijn@brixit.nl>
2016-08-10 15:35:52 +0200

    Added mod_wsgi support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 from app import app, db, celery
@@ -6,6 +7,9 @@ import indexer
 from auth import *
 from models import *
 from views import *
+
+# mod_wsgi support
+application = app
 
 if __name__ == '__main__':
     indexer.index()
