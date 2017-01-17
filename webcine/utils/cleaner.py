34257b19a6941e511db4d161e769041d4a2d4caa
Martijn Braam <martijn@brixit.nl>
2017-01-17 19:52:04 +0100

    Bugs

--- webcine/utils/cleaner.py
+++ webcine/utils/cleaner.py
@@ -1,11 +1,13 @@
 from webcine.models import Media
 from webcine.utils import ffmpeg
+import logging
 
 
 def remove_broken_media():
     """ Remove unplayable media files (video files containing no video data at all) """
     for media in Media.select().where(Media.length >> None):
         try:
+            logging.info('Processing file {}'.format(media.path))
             probe = ffmpeg.get_video_metadata(media.path)
             media.length = probe.length
             media.save()
