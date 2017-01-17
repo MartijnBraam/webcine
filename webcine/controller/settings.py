1acc0a0ba12b86cfee7008cf266a5726885ee31a
Martijn Braam <martijn@brixit.nl>
2017-01-17 13:36:57 +0100

    User table interactivity

--- webcine/controller/settings.py
+++ webcine/controller/settings.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, request
 
 from webcine.app import app
 from webcine.models import User
+import webcine.models
 from webcine.utils.auth import auth
 
 
@@ -10,3 +11,12 @@ from webcine.utils.auth import auth
 def admin_users():
     users = list(User.select())
     return render_template('settings/users.html', users=users)
+
+
+@app.route('/admin/save/<entity>', methods=['POST'])
+@auth.admin_required
+def admin_save_entity(entity):
+    entity = entity.title()
+    payload = request.json
+    entity = getattr(webcine.models, entity).get(getattr(webcine.models, entity).id == payload['id'])
+    return repr(entity)
