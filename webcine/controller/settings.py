8c2a174382b3cad31baa68aaaae34b69b1505329
Martijn Braam <martijn@brixit.nl>
2017-01-17 17:05:23 +0100

    Create user form

--- webcine/controller/settings.py
+++ webcine/controller/settings.py
@@ -32,13 +32,13 @@ def admin_save_user():
 @app.route('/admin/create/user', methods=['POST'])
 @auth.admin_required
 def admin_create_user():
-    name = request.form['name']
+    username = request.form['username']
     email = request.form['email']
     password = request.form['password']
     active = 'active' in request.form
     admin = 'admin' in request.form
 
-    user = User.create(name=name, email=email, active=active, admin=admin)
+    user = User.create(username=username, email=email, active=active, admin=admin)
     user.set_password(password)
     user.save()
     return redirect(url_for('admin_users'))
